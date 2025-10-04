c2d04237d761a69ba68b7e9f833223a48c511161
Haries Mugunthan G <mugunthang456@gmail.com>
2025-10-04 06:22:35 -0700

    Create Plus_One.java

---
+++ Math/Plus_One.java
@@ -0,0 +1,31 @@
+public class PlusOne {
+    public static void main(String[] args) {
+        int[] digits = {9, 9};
+
+        Solution sol = new Solution();
+        int[] result = sol.plusOne(digits);
+
+        System.out.print("[");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+}
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+}
